fc636f856fb1bd3a795b4613c2aafc2330f0ca35
Antonio José Masiá <email@antoniomasia.com>
2019-05-05 17:05:21 +0200

    Add image loader

--- config/loaders.js
+++ config/loaders.js
@@ -11,4 +11,16 @@ module.exports = {
     test: /\.less$/,
     use: ['style-loader', 'css-loader', 'less-loader'],
   },
+  urlLoader: {
+    test: /\.png$/,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: 30000,
+          name: 'assets/images/[name].[ext]',
+        },
+      },
+    ],
+  },
 };
