dbb5d319ff1d5d41bcb52631c486c3728274cef2
Antonio José Masiá <email@antoniomasia.com>
2019-05-05 14:19:53 +0200

    Rename config file and update bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = env => ({
   mode: env.NODE_ENV,
   entry: path.resolve(__dirname, 'src', 'entry.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
 });
